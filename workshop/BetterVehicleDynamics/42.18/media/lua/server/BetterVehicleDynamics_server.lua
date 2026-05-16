e7d1f34b5ff6b6162fc741caecb5cc8439ae7cf4
Justin Tagle <grphxprod@gmail.com>
2026-05-16 13:37:05 +0800

    P2 server: port BetterVehicleDynamics_server + P2.16 require/bridge audit

---
+++ workshop/BetterVehicleDynamics/42.18/media/lua/server/BetterVehicleDynamics_server.lua
@@ -0,0 +1,16 @@
+function BetterVehicleDynamicsVersionCheckServer()
+	if BetterVehicleDynamicsMod.javaVersion == "not installed" and not VersionCheckConfirmed then
+		--VersionCheckConfirmed = true; -- spam if not installed.
+		print("Better Vehicle Dynamics is not properly installed on your server. See Steam workshop page for details");
+	elseif BetterVehicleDynamicsMod.javaVersion ~= "3.4" and not VersionCheckConfirmed then
+		--VersionCheckConfirmed = true; -- spam if out of date
+		print("Better Vehicle Dynamics manual install is outdated on your server. Reinstall Zombie Folder to update to latest version. See Steam workshop page for details")
+	else
+		VersionCheckConfirmed = true;
+	end
+end
+
+if isServer() then
+	Events.OnSpawnVehicleEnd.Add(BetterVehicleDynamicsVersionCheckServer)
+	print("Better Vehicle Dynamics Server Side Loaded")
+end
